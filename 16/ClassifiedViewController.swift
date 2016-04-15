03c2794f9ee97422a6333ac9f5771bd024604f02
Yun <zcloud.zy@gmail.com>
2016-04-16 00:46:52 +0800

    1. Embedded in navigation controller. 2. 完成ClassifiedView布局

---
+++ 16/ClassifiedViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ClassifiedViewController.swift
+//  16
+//
+//  Created by zCloud on 4/15/16.
+//  Copyright © 2016 Yun.Zou. All rights reserved.
+//
+
+import UIKit
+
+class ClassifiedViewController: UIViewController {
+    
+    @IBOutlet var ownerAvatar: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        self.navigationController?.navigationBar.translucent = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
